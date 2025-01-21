06d708821e9904cd32b5ec3504d0afb49718a7ab
pankajvr <pankajsharma@valueresearch.in>
2025-01-21 14:41:22 +0530

    signup screen added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import { Messages } from "./pages/Messages"
 import Login from "./section/chat/auth/Login"
+import Signup from "./section/chat/auth/Signup"
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route index={true} element={<Messages />} />
       <Route path='/auth/login' element={<Login />} />
+      <Route path='/auth/signup' element={<Signup />} />
     </Routes>
   )
 }
