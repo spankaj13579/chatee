8d4520d483c919db1a21f0f612f65d2185732d22
pankajvr <pankajsharma@valueresearch.in>
2025-01-23 22:34:20 +0530

    added dark mode in signup and login also verificaiton page

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@ import { Messages } from "./pages/Messages"
 import Login from "./pages/auth/Login"
 import Signup from "./pages/auth/Signup"
 import Verification from "./pages/auth/Verification"
+import { useEffect } from "react"
 
 
 function App() {
+  useEffect(()=>{
+  const colorMode = JSON.parse(window.localStorage.getItem("color-theme"));
+  const className = "dark";
+  const bodyClass = window.document.body.classList;
+  colorMode === "dark" ? bodyClass.add(className) : bodyClass.remove(className);
+  },[]);
   return (
     <Routes>
       <Route index={true} element={<Messages />} />
