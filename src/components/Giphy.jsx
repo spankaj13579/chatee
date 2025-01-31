8bff5ea053f707d98303f962ea96f627d8fb3460
pankajvr <pankajsharma@valueresearch.in>
2025-01-31 14:19:58 +0530

    giphy component created and installed depandencies

---
+++ src/components/Giphy.jsx
@@ -0,0 +1,12 @@
+import React, { useRef } from 'react'
+
+function Giphy() {
+    const gridRef = useRef(null);
+  return (
+    <div ref={gridRef} className='w-full'>
+        
+    </div>
+  )
+}
+
+export default Giphy
